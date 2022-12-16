a84f98ca5088ac2c02d4013cfc28d8cda3e4f494
Ibaad Ahmed <ibaad.ahmed@rolustech.net>
2022-12-16 12:19:56 +0500

    opreation result changes

--- UniversityApp/UniversityApp/Models/OperationResult.cs
+++ UniversityApp/UniversityApp/Models/OperationResult.cs
@@ -16,5 +16,12 @@
             Message = message;
             Payload = payload;
         }
+        public static OperationResult Success() => new OperationResult(true, null);
+
+        public static OperationResult Success(string message, object payload = null) => new OperationResult(true, message, payload);
+
+        public static OperationResult Failure(string message, object payload = null) => new OperationResult(false, message, payload);
+
+        public static OperationResult Forbid(string message, object payload = null) => new OperationResult(false, message, payload);
     }
 }
